f18adb443f5b38feda3101577dbf02e764c56bab
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2024-08-07 07:22:23 +0300

    layout rough score component

--- components/GameArea/GameArea.jsx
+++ components/GameArea/GameArea.jsx
@@ -1,8 +1,9 @@
 import { View, ImageBackground, StyleSheet, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import background_image from "../../assets/images/Game/scene.jpg";
 import Bird from "../Bird/Bird";
 import Obstacle from "../Obstacle/Obstacle";
+import Scoreboard from "../ScoreBoard/ScoreBoard";
 
 const obstacles = [
   { position: { x: 550, y: 160 } },
@@ -12,6 +13,13 @@ const obstacles = [
 ];
 
 const GameArea = () => {
+  const [score, setScore] = useState(0);
+
+  // Example function to increase score (this could be tied to game logic)
+  const increaseScore = () => {
+    setScore(score + 10);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground source={background_image} style={styles.backgroundImage}>
@@ -21,6 +29,7 @@ const GameArea = () => {
         {obstacles.map((obs, index) => (
           <Obstacle key={index} position={obs.position} />
         ))}
+        <Scoreboard score={score} />
       </ImageBackground>
     </SafeAreaView>
   );
