c89beb4c2ecbc9c57cad98b276ee6b8dba224499
oceeboy <osieta33@gmail.com>
2024-08-08 04:13:05 -0400

    Add background music with expo-av
    - Implemented audio playback for background music using expo-av.
    - Configured music to loop and play automatically.
    - Added cleanup for audio on component unmount.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,29 +1,54 @@
-import { View, Text,Alert } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { View, Text, Alert } from "react-native";
 import { Slot } from "expo-router";
 import * as Orientation from "expo-screen-orientation";
 import * as NavigationBar from "expo-navigation-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as Updates from "expo-updates";
 import { Stack } from "expo-router";
+import { Audio } from "expo-av"; // Oceeboi added this import
+
 const RootLayout = () => {
+  const sound = useRef(new Audio.Sound()); // Oceeboi added this ref
+
+  useEffect(() => {
+    const loadAndPlayMusic = async () => {
+      try {
+        await sound.current.loadAsync(
+          require("../assets/audio/backgroundmusic.mp3"),
+          {
+            shouldPlay: true, // Automatically play when loaded
+            isLooping: true, // Loop the music
+          }
+        );
+      } catch (error) {
+        console.error("Error loading sound", error);
+      }
+    };
+
+    loadAndPlayMusic();
+
+    // Clean up sound on component unmount
+    return () => {
+      sound.current.unloadAsync();
+    };
+  }, []); // Oceeboi added this useEffect
+
   useEffect(() => {
     onFetchUpdateAsync();
     SwitchScreenToLandscape();
     HideNavigationBar();
   }, []);
 
-  //   change screen orientation to landscape
   const SwitchScreenToLandscape = async () => {
     await Orientation.lockAsync(Orientation.OrientationLock.LANDSCAPE_RIGHT);
   };
-  // hide navigation bar
+
   const HideNavigationBar = async () => {
     await NavigationBar.setVisibilityAsync("hidden");
     await NavigationBar.setBehaviorAsync("overlay-swipe");
   };
 
-  // check for updates
   async function onFetchUpdateAsync() {
     try {
       const update = await Updates.checkForUpdateAsync();
@@ -37,13 +62,13 @@ const RootLayout = () => {
         await Updates.reloadAsync();
       }
     } catch (error) {
-      // You can also add an alert() to see the error message in case of an error when fetching updates.
       alert(`Error fetching latest Expo update: ${error}`);
     }
   }
+
   return (
     <GestureHandlerRootView>
-      <Stack screenOptions={{ headerShown: false }} >
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="game" />
         <Stack.Screen name="index" />
       </Stack>
